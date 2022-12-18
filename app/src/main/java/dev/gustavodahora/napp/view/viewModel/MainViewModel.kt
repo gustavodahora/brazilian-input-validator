19f2519d324bcd2869dfa910e5aa6f9da44a940a
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2022-12-18 03:14:13 -0300

    Solving implementation sample

--- app/src/main/java/dev/gustavodahora/napp/view/viewModel/MainViewModel.kt
+++ app/src/main/java/dev/gustavodahora/napp/view/viewModel/MainViewModel.kt
@@ -1,12 +1,40 @@
 package dev.gustavodahora.napp.view.viewModel
 
-import androidx.lifecycle.MutableLiveData
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.gustavodahora.br_input_validation.ValidateInput
+import dev.gustavodahora.napp.view.model.FieldState
+import dev.gustavodahora.napp.view.model.FieldStatus
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-class MainViewModel: ViewModel() {
-    var toastStatus = MutableLiveData<Boolean>()
+class MainViewModel : ViewModel() {
+    private var validateJob: Job = Job()
+
+    var textCnpj by mutableStateOf(TextFieldValue(text = EMPTY_TEXT))
+    var textCpf by mutableStateOf(TextFieldValue(text = EMPTY_TEXT))
+
+    var state = mutableStateOf(FieldState())
 
     fun validate() {
-        toastStatus.value = true
+        validateJob.cancel()
+        validateJob = viewModelScope.launch {
+            when (ValidateInput.cnpj(textCnpj.text)) {
+                true -> {
+                    state.value = state.value.copy(cnpj = FieldStatus.VALID)
+                }
+                false -> {
+                    state.value = state.value.copy(cnpj = FieldStatus.ERROR)
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val EMPTY_TEXT = ""
     }
 }
